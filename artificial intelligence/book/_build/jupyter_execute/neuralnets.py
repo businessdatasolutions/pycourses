fc52ad67e02ed7383902fe42be09c3a2de8d67d0
witusj <wfth69@gmail.com>
2021-02-09 20:39:00 +0100

    Added chapter Neural Net

---
+++ artificial intelligence/book/_build/jupyter_execute/neuralnets.py
@@ -0,0 +1,101 @@
+# Neural Networks
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import tensorflow as tf
+from keras.layers import Dense
+from keras.models import Sequential
+
+$$
+   f:\mathbb R \to \mathbb R, \ y = 2x^2 - \frac{1}{3}x^3
+$$
+
+def f(x):
+    return 2 * x ** 2- x ** 3 / 3
+
+x = np.linspace(-2, 4, 25)
+x
+
+y = f(x)
+y
+
+
+plt.plot(x, y, 'ro')
+
+```{math}
+    f:\mathbb R \to \mathbb R, \ y = \alpha + \beta * x
+```
+
+beta = np.cov(x, y, ddof=0)[0,1] / np.var(x)
+beta
+
+alpha = y.mean() - beta * x.mean()
+alpha
+
+y_ = alpha + beta * x
+MSE = ((y - y_) ** 2).mean()
+MSE
+
+plt.plot(x, y, 'ro', label='sample data')
+plt.plot(x, y_, lw=3.0, label='linear regression')
+plt.legend()
+
+plt.plot(x, y, 'ro', label='sample data')
+for deg in [1, 2, 3]:
+    reg = np.polyfit(x, y, deg=deg)
+    y_ = np.polyval(reg, x)
+    MSE = ((y - y_) ** 2).mean()
+    print(f'deg={deg} | MSE={MSE:.5f}')
+    plt.plot(x, y_, label=f'deg={deg}')
+plt.legend()
+
+tf.random.set_seed(100)
+
+model = Sequential()
+model.add(Dense(256, activation='relu', input_dim=1))
+model.add(Dense(1, activation='linear'))
+model.compile(loss='mse', optimizer='rmsprop')
+
+plt.plot(x, y, 'ro', label='sample data')
+for _ in range(1, 6):
+    model.fit(x, y, epochs=100, verbose=False)
+    y_ = model.predict(x)
+    MSE = ((y-y_.flatten()) ** 2).mean()
+    print(f'round={_} | MSE={MSE:.5f}')
+    plt.plot(x, y_, '--', label=f'round={_}')
+plt.legend()
+
+
+np.random.seed(0)
+x = np.linspace(-1, 1)
+y = np.random.random(len(x)) * 2 - 1
+
+plt.plot(x, y, 'ro', label='sample data')
+for deg in [1, 5, 9, 11, 13, 15]:
+    reg = np.polyfit(x, y, deg=deg)
+    y_ = np.polyval(reg, x)
+    MSE = ((y - y_) ** 2).mean()
+    print(f'deg={deg:2d} | MSE={MSE:.5f}')
+    plt.plot(x, y_, label=f'deg={deg}')
+plt.legend()
+
+
+model = Sequential()
+model.add(Dense(256, activation='relu', input_dim=1))
+for _ in range(3):
+    model.add(Dense(256, activation='relu'))
+model.add(Dense(1, activation='linear'))
+model.compile(loss='mse', optimizer='rmsprop')
+
+model.summary()
+
+plt.plot(x, y, 'ro', label='sample data')
+for _ in range(1, 8):
+    model.fit(x, y, epochs=500, verbose=False)
+    y_ = model.predict(x)
+    MSE = ((y-y_.flatten()) ** 2).mean()
+    print(f'round={_} | MSE={MSE:.5f}')
+    plt.plot(x, y_, '--', label=f'round={_}')
+plt.legend()
+
